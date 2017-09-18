eb00b21124b95d766e7727cf7a70b91a139f5ff4
Indrashish Ghosh <mail@indrashishghosh.com>
2017-09-19 00:19:38 +0530

    Updated reload directory

--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,9 @@
-module.exports = () => ({
-  // map: ctx.options.map,
-  // parser: ctx.file.extname === '.sss' ? 'sugarss' : false,
-  // plugins: {
-  //   'postcss-import': { root: ctx.file.dirname },
-  //   'cssnano': ctx.env === 'production' ? {} : false
-  // }
+module.exports = (ctx) => ({
   parser: 'postcss-scss',
   plugins: {
     'postcss-import': {
       root: process.cwd()
     },
-    'cssnano': {
-      preset: 'default',
-    }
+    'cssnano': ctx.env === 'production' ? {preset: 'default'} : false
   }
 })
