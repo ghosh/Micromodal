93ec566faf4d636ffbf3afd1e0e3f7c527350bb1
Indrashish Ghosh <mail@indrashishghosh.com>
2019-07-22 02:14:08 +0530

    Init next

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = (ctx) => ({
-  parser: 'postcss-scss',
-  plugins: {
-    'postcss-import': {
-      root: process.cwd()
-    },
-    'cssnano': ctx.env === 'production' ? {preset: 'default'} : false
-  }
-})
